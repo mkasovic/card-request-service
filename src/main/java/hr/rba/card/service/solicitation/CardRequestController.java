96f5550a2e5c9e884032a071254cec4d77240835
Marko Kasović <mkasovic@gmail.com>
2024-10-23 18:22:17 +0200

    Cleaned-up annotations

--- src/main/java/hr/rba/card/service/solicitation/CardRequestController.java
+++ src/main/java/hr/rba/card/service/solicitation/CardRequestController.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/card-requests")
-@Transactional
 @AllArgsConstructor
 public class CardRequestController {
     private final CardRequestService service;
